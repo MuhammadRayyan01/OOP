0ddfbf136b7746cdec4898b5f6117b0bab1f2e47
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-08-27 19:24:12 +0700

    feat: OOP concept: Motorcycle Inheritance from Bicycle

---
+++ week/one/assignment/BikeDemo.java
@@ -0,0 +1,28 @@
+package week.one.assignment;
+
+public class BikeDemo{
+    public static void main(String[] args) {
+        Bike mountainBike1 = new Bike();
+        Bike mountainBike2 = new Bike();
+        Motorcycle motor1 = new Motorcycle();
+
+    mountainBike1.setBrand("trek");
+    mountainBike1.speedAcc(10);
+    mountainBike1.gearChanges(2);
+    mountainBike1.printInfo();
+
+    mountainBike2.setBrand("Giant");
+    mountainBike2.speedAcc(20);
+    mountainBike2.gearChanges(3);
+    mountainBike2.printInfo();
+
+    motor1.setBrand("Nmax");
+    motor1.speedAcc(30);
+    motor1.gearChanges(2);
+    motor1.setMachineType("Matic");
+    motor1.printInfo();
+
+
+    }
+
+}
---
+++ week/one/assignment/Motorcycle.java
@@ -0,0 +1,18 @@
+package week.one.assignment;
+
+public class Motorcycle extends Bike{
+  private String MachineType;
+  public void setMachineType(String Machine){
+    MachineType = Machine;
+  }
+  @Override
+  public void printInfo(){
+    super.printInfo();
+    System.out.println("Machine Type: "+ MachineType);
+    System.out.println("Bike Development: Motorcycle");
+  }
+
+  
+
+
+}

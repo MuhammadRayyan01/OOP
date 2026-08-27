026de24b48232ef930b60b85be60bd49fcca44b6
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-08-27 14:49:08 +0700

    feat: 1st lab

---
+++ week/one/BikeDemo.java
@@ -0,0 +1,18 @@
+package week.one;
+public class BikeDemo{
+    public static void main(String[] args) {
+        Bike mountainBike1 = new Bike();
+        Bike mountainBike2 = new Bike();
+        
+    mountainBike1.setBrand("trek");
+    mountainBike1.speedAcc(10);
+    mountainBike1.gearChanges(2);
+    mountainBike1.printInfo();
+
+    mountainBike2.setBrand("Giant");
+    mountainBike2.speedAcc(20);
+    mountainBike2.gearChanges(3);
+    mountainBike2.printInfo();
+    }
+
+}

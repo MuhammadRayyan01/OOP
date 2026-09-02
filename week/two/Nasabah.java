bcd14748fb2a0e6c84e52cfe209f8b7483174233
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-02 07:56:19 +0700

    feat: basic OOP object

---
+++ week/two/Nasabah.java
@@ -0,0 +1,10 @@
+package week.two;
+
+public class Nasabah {
+    String nama;
+    int Saldo;
+
+    public void tampilkanSaldo(){
+    System.out.println("nama:"+nama+"\n saldonya:"+Saldo);
+    }
+}
---
+++ week/two/NasabahMain.java
@@ -0,0 +1,15 @@
+package week.two;
+
+public class NasabahMain {
+    public static void main(String[] args) {
+        Nasabah N1= new Nasabah();
+        Nasabah N2 = new Nasabah();
+        N1.nama="Didi";
+        N1.Saldo=20200;
+        N1.tampilkanSaldo();
+        N2.nama="Dada";
+        N2.Saldo=10101;
+        N2.tampilkanSaldo();
+    }
+    
+}

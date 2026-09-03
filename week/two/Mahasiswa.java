75bcfdef8f07ab33a685e12fe2c330dc65e113f3
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-03 13:12:07 +0700

    feat: creating mahasiswa simple program

---
+++ week/two/Mahasiswa.java
@@ -0,0 +1,15 @@
+package week.two;
+
+public class Mahasiswa {
+    int nim;
+    String name;
+    String address;
+    String classroom;
+    
+    public void viewBiodata(){
+        System.out.println("nim: "+nim);
+        System.out.println("name: "+name);
+        System.out.println("address: "+address);
+        System.out.println("classroom: "+classroom);
+    }
+}
---
+++ week/two/testMahasiswa.java
@@ -0,0 +1,13 @@
+package week.two;
+
+
+public class testMahasiswa {
+    public static void main(String[] args) {
+        Mahasiswa mhs1=new Mahasiswa();
+        mhs1.nim=121;
+        mhs1.name="icikiwir";
+        mhs1.address="Jl.Soekarno";
+        mhs1.classroom="1A";
+        mhs1.viewBiodata();
+    }
+}

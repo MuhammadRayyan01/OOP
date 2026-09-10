e9d58565c7af73e1420f8dec618db53319367558
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-10 15:30:35 +0700

    feat: motor program

--- week/three/Motor.java
+++ week/three/Motor.java
@@ -1,8 +1,30 @@
 package week.three;
 
 public class Motor {
-    private int kecepatan=0;
-    public boolean kontakOn=false;
+    private  int kecepatan=0;
+    private  boolean kontakOn=false;
+
+    public void nyalakanMesin(){
+        kontakOn=true;
+    }
+    public void matikanMesin(){
+        kontakOn=false;
+        kecepatan=0;
+    }
+    public void tambahKecepatan(){
+        if(kontakOn==true){
+            kecepatan+=5;
+        }
+        else{
+        System.out.println("kecepatan tidak bisa bertambah karena mesin off"); 
+        }
+    }
+    public void kurangiKecepatan(){
+        if(kontakOn==true){
+        }else{
+            System.out.println("kecepatan tidak bisa berkurang karena mesin off");
+        }
+    }
     public void printStatus(){
         if(kontakOn==true){
             System.out.println("kontak on");
@@ -10,7 +32,12 @@ public class Motor {
         }else{
             System.out.println("kontak off");
 
-        }
-        System.out.println("kecepatan "+ kecepatan+"\n");
+        } if(kecepatan<100){
+            kecepatan+=90;
+            System.out.println("kecepatan "+ kecepatan+"\n");
+
+        }else{
+        System.out.println("speed is max");
+    }
     }
 }
--- week/three/MotorDemo.java
+++ week/three/MotorDemo.java
@@ -4,7 +4,24 @@ public class MotorDemo {
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan=50;
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
         motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        
+        motor.matikanMesin();
+        motor.tambahKecepatan();
+
+        
     }
 }

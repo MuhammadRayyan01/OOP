3daa7b60bd2ee24ca527a28d7f414d98b2813014
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-12 22:21:48 +0700

    feat: adding a scanner feature

--- week/three/task/TestLogistik.java
+++ week/three/task/TestLogistik.java
@@ -1,23 +1,28 @@
 package week.three.task;
+import java.util.Scanner;
 
 
 public class TestLogistik {
     public static void main(String[] args) {
-        Container kontainerA = new Container("2001","PT W",5000,0);
+    Scanner sc=new Scanner(System.in);
+    Container kontainerA = new Container("2001","PT W",5000,0);
+
+        
 
         System.out.println("Container owner name: "+kontainerA.getName());
         System.out.println("Max Capacity: "+kontainerA.getCap()+ " kg");
 
-        System.out.println("memasukkan muatan baru seberat 6000kg");
-        kontainerA.setCap(6000);
+        System.out.print("memasukkan muatan baru dengan berat:");
+        int weight = sc.nextInt();
+        kontainerA.setCap(weight); 
         System.out.println("berat muatan saat ini "+kontainerA.getCap()+ " kg");
 
         System.out.println("memasukkan muatan baru seberat 4000kg");
         kontainerA.setCap(4000);
         System.out.println("berat muatan saat ini: "+kontainerA.getCap()+ " kg");
 
-        System.out.println("menurunkan berat 500 kg");
-        kontainerA.decreaseWeight(500);
+        System.out.println("menurunkan berat 5050kg");
+        kontainerA.decreaseWeight(50050);
         System.out.println("berat muatan saat ini: "+ kontainerA.getCap());
 
     }

b9b5158eb53d8551a3be8718f1b6e278c8316908
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-20 10:56:36 +0700

    Feat: creating relation class exercise Lab 1

---
+++ week/o4/exp01/MainExperiment1.java
@@ -0,0 +1,9 @@
+package week.o4.exp01;
+
+public class MainExperiment1 {
+    public static void main(String[] args) {
+    Processor p = new Processor("Intel i5", 3);
+    Laptop l = new Laptop("Thinkpad", p);
+    l.info();
+    }
+}

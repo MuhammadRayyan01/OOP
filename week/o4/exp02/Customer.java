f9dcab5dc80edc7320a233c1992f9b36e4eab9ad
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-20 13:23:29 +0700

    feat: class relation Lab 2

---
+++ week/o4/exp02/Customer.java
@@ -0,0 +1,43 @@
+package week.o4.exp02;
+
+public class Customer {
+    private String name;
+    private Car car;
+    private Driver driver;
+    private int days;
+
+    public Customer() {
+    }
+
+    public void setName(String name) {
+    this.name = name;
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public void setCar(Car car) {
+    this.car = car;
+    }
+
+    public Car getCar() {
+    return car;
+    }
+
+    public void setDriver(Driver driver) {
+    this.driver = driver;
+    }
+
+    public Driver getDriver() {
+    return driver;
+    }
+
+    public void setDays(int days) {
+    this.days = days;
+    }
+
+    public int getDays() {
+    return days;
+    }
+}

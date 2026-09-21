8a4222a5813cd175c63370fb98db7c9e2e7dedc3
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-21 14:12:56 +0700

    feat: relation class 3rd experiment

---
+++ week/o4/exp03/Pegawai.java
@@ -0,0 +1,36 @@
+package week.o4.exp03;
+
+public class Pegawai {
+    private String nip;
+    private String nama;
+    private Pegawai masinis;
+    private Pegawai asisten;
+
+    public Pegawai(String nip, String nama) {
+    this.nip = nip;
+    this.nama = nama;
+    }
+
+    public void setNip(String nip) {
+    this.nip = nip;
+    }
+
+    public String getNip() {
+    return nip;
+    }
+
+    public void setNama(String nama) {
+    this.nama = nama;
+    }
+
+    public String getNama() {
+    return nama;
+    }
+    public String info() {
+    String info = "";
+    info += "Nip: " + this.nip + "\n";
+    info += "Nama: " + this.nama + "\n";
+    return info;
+    }
+}
+
